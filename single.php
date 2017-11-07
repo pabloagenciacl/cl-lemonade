f40b3868a74f30dff9be1007e7dc0c2b2806ad42
Pablo <pablo@couvelimao.com>
2017-11-07 13:05:23 -0200

    imagem post

--- single.php
+++ single.php
@@ -6,14 +6,18 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <div class="cl-banner-post" style="background-color: <?php echo get_color_category();?>;">
+    <section class="cl-banner-post" style="background-color: <?php echo get_color_category(); ?>;">
+        <div class="cl-banner-thumbnail">
+            <img src="<?php the_post_thumbnail_url(); ?>" alt="Tendências do Marketing Online em que todo Empreendedor Precisa Investir: 3 Grandes Áreas em Ascensão">
+        </div>
+
             <div class="large-12 columns">
                 <h1><?php the_title(); ?></h1>
             </div>
 
 
         <p><em><?php echo ucfirst(get_the_time('l, j \d\e F \d\e Y')); ?></em></p>
-    </div>
+    </section>
     <div class="entry-content">
         <?php the_content(); ?>
     </div>
