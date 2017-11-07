3584bbd438b341266e7d8ac279b2a4f205b1d3e6
jvoliveira <jvoliveira01@gmail.com>
2017-11-07 12:08:14 -0200

    alteração jumbotron

--- single.php
+++ single.php
@@ -4,30 +4,25 @@
 
 
 
-<div class="row">
-    <div class="col-md-12">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-
-            <div class="row cl-banner-post" style="background-color: <?php echo get_color_category();?>;">
-                <div class="large-12 columns">
-                    <h1><?php the_title(); ?></h1>
-                </div>
+    <div class="jumbotron" style="background-color: <?php echo get_color_category();?>;">
+            <div class="large-12 columns">
+                <h1><?php the_title(); ?></h1>
             </div>
 
-            
-            <p><em><?php echo ucfirst(get_the_time('l, j \d\e F \d\e Y')); ?></em></p>
-            <div class="entry-content">
-                <?php the_content(); ?>
-            </div>
-            <hr>
-            <?php comments_template(); ?>
 
-        <?php endwhile; else: ?>
-            <p><?php _e('Desculpe, essa página não existe.'); ?></p>
-        <?php endif; ?>
-
+        <p><em><?php echo ucfirst(get_the_time('l, j \d\e F \d\e Y')); ?></em></p>
     </div>
-</div>
+    <div class="entry-content">
+        <?php the_content(); ?>
+    </div>
+    <hr>
+    <?php comments_template(); ?>
+
+<?php endwhile; else: ?>
+<p><?php _e('Desculpe, essa página não existe.'); ?></p>
+<?php endif; ?>
+
 
 <?php get_footer(); ?>
