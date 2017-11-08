99e569888b9557476f01c3241a92f00f2177e2e6
jvoliveira <jvoliveira01@gmail.com>
2017-11-08 17:35:31 -0200

    Página do post beta

--- single.php
+++ single.php
@@ -6,34 +6,59 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <section class="cl-banner-post" style="background-color: <?php echo get_color_category(); ?>;">
-        <img class="cl-banner-thumbnail" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
-        <div class="large-12 columns">
-            <div class="cont-titulo">
-                <h4><?php get_categoria(); ?> </h4>
-                <h2><?php the_title(); ?></h2>
+    <section class="cl-banner-post col-md-12" style="background-color: <?php echo get_color_category(); ?>;">
 
+        <img class="cl-banner-thumbnail" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+        
+        <div class="cont-titulo col-md-12">
+
+            <h4><?php get_categoria(); ?> </h4>
+            <h2><?php the_title(); ?></h2>
+            <div class="autor col-md-4">
                 <?php echo get_avatar( get_the_author_meta('user_email'), $size = '50', $default, $alt, array( 'class' => 'avt' ) ); ?>
-                <h3>Escrito por: <span class="neg"><?php the_author_meta( 'display_name'); ?></span><br><br><em><?php echo ucfirst(get_the_time('l, j \d\e F \d\e Y')); ?></em></h3>    
-            </div>
+                <h3>Escrito por: <span class="neg"><?php the_author_meta( 'display_name'); ?></span><br><em><?php echo ucfirst(get_the_time('l, j \d\e F \d\e Y')); ?></em></h3>
+            </div>    
         </div>
     </section>
     <section class="cta1">
-        <h5>Junte-se a mais de 150.000 <br> pessoas!</h5>
-        <h3>Entre para nossa lista e receba conteúdos exclusivos!</h3>
         <div class="col-md-6">
-            <form align="center">
-                    <input class="tf-email" type="email" name="email" placeholder="Qual seu melhor email?" value="" required="">
-                    <input type="submit"  value="Cadastrar email" class="bt-env">
-            </form>
+            <h5>Junte-se a mais de 150.000 pessoas!</h5>
+            <h3>Entre para nossa lista e receba conteúdos exclusivos!</h3>
         </div>
+        
+        
+        <form class="col-md-6 femail">
+            <div class="col-md-12">
+                <input id="tf-email-txt" class="tf-email" placeholder="&#xf003; Seu email" value="">
+                <input type="submit"  value="Cadastrar" class="bt-env">
+            </div>
+        </form>
+        
     </section>
-    <div class="entry-content">
+    <div class="entry-content col-md-12">
         <?php the_content(); ?>
     </div>
     <hr>
-    <?php comments_template(); ?>
-
+    <div class="comentarios">
+       <?php if (comments_open()) : ?>
+        <div id="disqus_thread"></div>
+        <script>
+            var disqus_config = function () {
+                this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+            };
+            (function() {  // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                
+                s.src = '//couve-limao.disqus.com/embed.js';
+                
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
+        <noscript>Por favor habilite o JavaScript para ver os <a href="http://disqus.com/?ref_noscript">comentários.</a></noscript>
+    <?php endif; // comments_open ?>
+</div>
 <?php endwhile; else: ?>
 <p><?php _e('Desculpe, essa página não existe.'); ?></p>
 <?php endif; ?>
